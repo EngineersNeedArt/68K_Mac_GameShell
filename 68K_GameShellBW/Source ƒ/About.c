96f5924390a3fd21165d05a151900ec882d82096
John Calhoun <84056606+EngineersNeedArt@users.noreply.github.com>
2026-09-07 07:02:20 -0500

    Format code for consistency in About.c

--- 68K_GameShellBW/Source ƒ/About.c
+++ 68K_GameShellBW/Source ƒ/About.c
@@ -32,28 +32,28 @@ void doAbout (Boolean doZooms) {
 	
 	// Center and load the dialog.
 	centerDialog(kAboutDialogID);
-	aboutDialog = GetNewDialog(kAboutDialogID, NULL, (WindowPtr)-1);
+	aboutDialog = GetNewDialog (kAboutDialogID, NULL, (WindowPtr)-1);
 	if (!aboutDialog) {
-		fatalError(kErrFailedResourceLoad);
+		fatalError (kErrFailedResourceLoad);
 		return;
 	}
 	
-	ParamText("\pGameShellC", "\pNemo", "\pv. 1.0", "\p");
+	ParamText ("\pGameShellC", "\pNemo", "\pv. 1.0", "\p");
 	
 	// Show the dialog, make it the current graphics port.
-	GetPort(&oldPort);
-	ShowWindow((GrafPtr)aboutDialog);
-	SetPort((GrafPtr)aboutDialog);
+	GetPort (&oldPort);
+	ShowWindow ((GrafPtr)aboutDialog);
+	SetPort ((GrafPtr)aboutDialog);
 	
-	drawDefaultButton(aboutDialog, 1);
+	drawDefaultButton (aboutDialog, 1);
 	
 	// Loop until the OK button is hit.
 	while (itemHit == 0) {
-		ModalDialog(NULL, &itemHit);
+		ModalDialog (NULL, &itemHit);
 	}
 	
 	// Finished.
-	DisposeDialog(aboutDialog);
-	SetPort(oldPort);
+	DisposeDialog (aboutDialog);
+	SetPort (oldPort);
 }
 
